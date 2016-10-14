2968b14fa1e57a37836c561c30a066981831701e
Marko Stijak <mstijak@gmail.com>
2016-10-14 16:07:25 +0200

    ...

--- config/babel.config.js
+++ config/babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "cacheDirectory": true,
-    "cacheIdentifier": "v6",
+    "cacheIdentifier": "v7",
     "presets": ["es2015-loose", "stage-0"],
     "plugins": [
         ["babel-plugin-transform-react-jsx", {"pragma": "VDOM.createElement"}],
