df7318613ebec6b96471ce7cf13ab0ab34a19f29
Marko Stijak <mstijak@gmail.com>
2016-10-19 08:43:53 +0200

    ...

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,34 +12,36 @@ import Help from './help';
 import Controller from './Controller';
 
 export default <cx>
-    <div class="cxb-layout" controller={Controller}>
-        <header class="cxe-layout-header">
-            <h1>tdo</h1>
-            <TextField value:bind="search.query" placeholder="Search..." mod="search" />
+    <Sandbox key:bind="hash" storage:bind="pages" layout={FirstVisibleChildLayout} immutable>
+        <div class="cxb-layout" controller={Controller}>
+            <header class="cxe-layout-header">
+                <h1>tdo</h1>
+                <TextField value:bind="search.query" placeholder="Search..." mod="search"/>
 
-            <Repeater
-                records:bind="tdo.boards"
-                filter={b=>!b.deleted}
-            >
-                <a href:tpl="#{$record.id}"
-                   class={{active: {expr: '{hash}=="#" + {$record.id}'}}}
-                   style:bind="$record.headerStyle"
-                   text:bind="$record.name" />
-            </Repeater>
-            <a href="#" onClick="addBoard">Add Board</a>
+                <Repeater
+                    records:bind="tdo.boards"
+                    filter={b=>!b.deleted}
+                >
+                    <a href:tpl="#{$record.id}"
+                       class={{active: {expr: '{hash}=="#" + {$record.id}'}}}
+                       style:bind="$record.headerStyle"
+                       text:bind="$record.name"
+                    />
+                </Repeater>
+                <a href="#" onClick="addBoard">Add Board</a>
 
-            <div class="spacer" />
+                <div class="spacer"/>
 
-            <a href="#settings" class={{active: {expr: '{hash}=="#settings"'}}}>Settings</a>
-            <a href="#help" class={{active: {expr: '{hash}=="#help"'}}}>Help</a>
+                <a href="#settings" class={{active: {expr: '{hash}=="#settings"'}}}>Settings</a>
+                <a href="#help" class={{active: {expr: '{hash}=="#help"'}}}>Help</a>
 
-            <div class="spacer" />
+                <div class="spacer"/>
 
-            <a href="https://github.com/mstijak/tdo" target="_blank">Report/Suggest</a>
-            <a href={`http://twitter.com/home?status=${encodeURIComponent("tdo - hackable todo list #todo https://goo.gl/rhkuYP")}`} target="_blank">Tweet</a>
-        </header>
-        <main class="cxe-layout-main">
-            <Sandbox key:bind="hash" storage:bind="pages" layout={FirstVisibleChildLayout} immutable>
+                <a href="https://github.com/mstijak/tdo" target="_blank">Report/Suggest</a>
+                <a href={`http://twitter.com/home?status=${encodeURIComponent("tdo - hackable todo list #todo https://goo.gl/rhkuYP")}`}
+                   target="_blank">Tweet</a>
+            </header>
+            <main class="cxe-layout-main" layout={FirstVisibleChildLayout}>
                 <Route url:bind="hash" route="#settings">
                     <Settings />
                 </Route>
@@ -49,7 +51,7 @@ export default <cx>
                 <Route url:bind="hash" route="#(:boardId)">
                     <Default />
                 </Route>
-            </Sandbox>
-        </main>
-    </div>
+            </main>
+        </div>
+    </Sandbox>
 </cx>;
